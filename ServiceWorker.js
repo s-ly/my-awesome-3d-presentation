d723c18fd3bfe36ec112643725d9ea9e90f1a8d7
s-ly <sergeylysov@outlook.com>
2026-04-10 05:01:28 +0300

    rotCam v5

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-Tmp_position-0.1";
+const cacheName = "DefaultCompany-Tmp_position-5";
 const contentToCache = [
     "Build/Build.loader.js",
     "Build/Build.framework.js",
